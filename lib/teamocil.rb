2d012a241b3992f57eceabef589577488bcd9c80
Rémi Prévost <remi@exomel.com>
2011-02-05 13:34:46 -0500

    Update version to 0.1.1

--- lib/teamocil.rb
+++ lib/teamocil.rb
@@ -4,6 +4,6 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Teamocil
-  VERSION = '0.1'
+  VERSION = '0.1.1'
   autoload :Layout, "teamocil/layout"
 end
