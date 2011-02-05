9f1b573a9989dc1f405572d86f2c451e51e9e839
Rémi Prévost <remi@exomel.com>
2011-02-05 16:38:07 -0500

    Add “lib” to $: in bin/teamocil

--- lib/teamocil.rb
+++ lib/teamocil.rb
@@ -1,8 +1,3 @@
-require 'yaml'
-
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 module Teamocil
   VERSION = '0.1.2'
   autoload :Layout, "teamocil/layout"
