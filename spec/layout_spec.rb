a424684c5a493505541636192230e9d6238ab824
Rémi Prévost <remi@exomel.com>
2011-10-23 17:43:48 -0400

    Improve specs

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -3,10 +3,23 @@ require File.join(File.dirname(__FILE__), "spec_helper.rb")
 describe Teamocil::Layout do
   context "initializing" do
 
-    it "create two windows" do # {{{
+    it "creates windows" do # {{{
       layout = Teamocil::Layout.new(layouts["two-windows"], {})
       commands = layout.generate_commands
-      commands.grep(/new-window/).length.should be 2
+      commands.grep(/new-window/).length.should == 2
+    end # }}}
+
+    it "renames the current session" do # {{{
+      layout = Teamocil::Layout.new(layouts["two-windows-in-a-session"], {})
+      commands = layout.generate_commands
+      commands.grep(/rename-session/).first.should == "tmux rename-session \"my-new-session\""
+      commands.grep(/new-window/).length.should == 2
+    end # }}}
+
+    it "creates splits" do # {{{
+      layout = Teamocil::Layout.new(layouts["four-splits"], {})
+      commands = layout.generate_commands
+      commands.grep(/split-window/).length.should == 3
     end # }}}
 
   end
