b9c80661d82dffd0b69c14e24f5ad249073adec0
Rémi Prévost <remi@exomel.com>
2012-03-09 22:08:40 -0500

    Improve CLI spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,14 +1,15 @@
+# encoding: utf-8
 require File.join(File.dirname(__FILE__), "spec_helper.rb")
 
 describe Teamocil::CLI do
 
   context "executing" do
 
-    context "not in tmux" do
-
       before do # {{{
         @fake_env = { "TMUX" => 1, "HOME" => File.join(File.dirname(__FILE__), "fixtures") }
       end # }}}
+
+    context "not in tmux" do
 
       it "should allow editing" do # {{{
         FileUtils.stub(:touch)
@@ -16,20 +17,39 @@ describe Teamocil::CLI do
 
         @cli = Teamocil::CLI.new(["--edit", "my-layout"], @fake_env)
       end # }}}
+
     end
 
     context "in tmux" do
 
-      before do # {{{
-        @fake_env = { "TMUX" => 1, "HOME" => File.join(File.dirname(__FILE__), "fixtures") }
+      before :each do # {{{
+        Teamocil::CLI.messages = []
       end # }}}
 
-      it "creates a layout" do # {{{
+      it "creates a layout from a name" do # {{{
         @cli = Teamocil::CLI.new(["sample"], @fake_env)
         @cli.layout.session.name.should == "sample"
         @cli.layout.session.windows.length.should == 2
         @cli.layout.session.windows.first.name.should == "foo"
         @cli.layout.session.windows.last.name.should == "bar"
+      end # }}}
+
+      it "fails to create a layout from a layout that doesn’t exist" do # {{{
+        lambda { @cli = Teamocil::CLI.new(["i-do-not-exist"], @fake_env) }.should raise_error SystemExit
+        Teamocil::CLI.messages.should include("There is no file \"#{File.join(File.dirname(__FILE__), "fixtures", ".teamocil", "i-do-not-exist.yml")}\"")
+      end # }}}
+
+      it "creates a layout from a specific file" do # {{{
+        @cli = Teamocil::CLI.new(["--layout", "./spec/fixtures/.teamocil/sample.yml"], @fake_env)
+        @cli.layout.session.name.should == "sample"
+        @cli.layout.session.windows.length.should == 2
+        @cli.layout.session.windows.first.name.should == "foo"
+        @cli.layout.session.windows.last.name.should == "bar"
+      end # }}}
+
+      it "fails to create a layout from a file that doesn’t exist" do # {{{
+        lambda { @cli = Teamocil::CLI.new(["--layout", "./spec/fixtures/.teamocil/i-do-not-exist.yml"], @fake_env) }.should raise_error SystemExit
+        Teamocil::CLI.messages.should include("There is no file \"./spec/fixtures/.teamocil/i-do-not-exist.yml\"")
       end # }}}
 
       it "lists available layouts" do # {{{
--- spec/mock/cli.rb
+++ spec/mock/cli.rb
@@ -5,10 +5,28 @@ module Teamocil
       def self.included(base) # {{{
         base.class_eval do
 
+          # Return all messages
+          def self.messages # {{{
+            @@messages
+          end # }}}
+
+          # Change messages
+          def self.messages=(messages) # {{{
+            @@messages = messages
+          end # }}}
+
           # Do not print anything
-          def print_layouts
+          def print_layouts # {{{
             # Nothing
-          end
+          end # }}}
+
+          # Print an error message and exit the utility
+          #
+          # @param msg [Mixed] something to print before exiting.
+          def bail(msg) # {{{
+            Teamocil::CLI.messages << msg
+            exit 1
+          end # }}}
 
         end
       end # }}}
