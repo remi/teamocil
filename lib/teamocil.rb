7caf4a9adc2b41c2c855bd74c3a83b459a61fbbe
Rémi Prévost <remi@exomel.com>
2013-03-28 13:13:46 -0400

    Bump version to 0.4.2

--- lib/teamocil.rb
+++ lib/teamocil.rb
@@ -1,7 +1,7 @@
+require "teamocil/version"
 require "teamocil/layout"
 require "teamocil/cli"
 require "teamocil/error"
 
 module Teamocil
-  VERSION = "0.4.1"
 end
---
+++ lib/teamocil/version.rb
@@ -0,0 +1,3 @@
+module Teamocil
+  VERSION = "0.4.2"
+end
