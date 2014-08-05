4ce161f2af8fb74081b4c22b9c5da87157cde054
Rémi Prévost <remi@exomel.com>
2014-08-05 09:08:32 -0400

    Fix a few rubocop errors

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ require 'bundler/gem_tasks'
 
 desc 'Start an IRB session with the gem'
 task :console do
-  $:.unshift File.expand_path('..', __FILE__)
+  $LOAD_PATH.unshift File.expand_path('..', __FILE__)
   require 'teamocil'
   require 'irb'
 
--- lib/teamocil/utils/closed_struct.rb
+++ lib/teamocil/utils/closed_struct.rb
@@ -1,12 +1,12 @@
 module Teamocil
   class ClosedStruct < Struct
-    def initialize *args
+    def initialize(*args)
       args = [{}] unless args.any?
 
       args.first.each_pair do |key, value|
         # Make sure we only set values to defined arguments
         if members.map { |x| x.intern }.include?(key.to_sym)
-          self.send "#{key}=", value
+          send "#{key}=", value
         else
           raise ArgumentError, "#{self.class.name} doesn’t support the `#{key}` keyword, only #{members.join(', ')}"
         end
