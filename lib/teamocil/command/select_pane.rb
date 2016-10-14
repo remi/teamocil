42a404720126d9684ddfa4cd63ee6694167525f4
Garrett Robinson <garrett.f.robinson@gmail.com>
2016-10-14 16:25:01 -0700

    Quote index of selected pane
    This fixes pane focusing in cases where the window name and/or the
    pane name contain spaces.

--- lib/teamocil/command/select_pane.rb
+++ lib/teamocil/command/select_pane.rb
@@ -2,7 +2,7 @@ module Teamocil
   module Command
     class SelectPane < ClosedStruct.new(:index)
       def to_s
-        "select-pane -t #{index}"
+        "select-pane -t '#{index}'"
       end
     end
   end
